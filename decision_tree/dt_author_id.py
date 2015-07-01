595e93d6c93e75732f12010ab6757d6bb11b25d7
vincent <vince_w_chiu_gmail_com@instance-1.c.plasma-amphora-95115.internal>
2015-07-01 05:00:48 +0000

    decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -26,7 +26,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
 
+#print number of features in the data
+print len(features_train[0])
+
+#train and scoring the algorithm
+clf = DecisionTreeClassifier(min_samples_split=40.)
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
 
 #########################################################
 
