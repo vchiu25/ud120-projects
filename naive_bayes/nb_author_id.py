4f20fd0385f184e3775513515851369ccd4ef068
vincent <vince_w_chiu_gmail_com@instance-1.c.plasma-amphora-95115.internal>
2015-07-01 03:18:34 +0000

    naive baye

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,8 +27,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
 #########################################################
 
 
