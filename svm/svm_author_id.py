6e6339ab9a6c21db318af1f07c8ce05d806383c4
vincent <vince_w_chiu_gmail_com@instance-1.c.plasma-amphora-95115.internal>
2015-07-01 03:20:13 +0000

    first commit

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
 
+#smaller dataset
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+
+#training and testing
+for c_parameter in [1]:
+    clf = SVC(kernel="rbf", C = c_parameter)
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "training time: ", round(time()-t0,2), "s"
+    t0 = time()
+    print clf.score(features_test, labels_test)
+    print "testing time: ", round(time()-t0,2), "s"
+
+
+#print result
+prediction = clf.predict(features_test)
+for i in [10,26,50]:
+    print prediction[i]
+
+print sum(prediction)
 #########################################################
 
 
